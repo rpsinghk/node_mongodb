04092c1f8df1d2d54ba032e6065dce02b12e9800
Ram Prakash Singh <ramprakash.singh@Pearson.com>
2020-05-15 16:49:03 +0530

    lodash & config added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const _ = require('lodash');
+const config = require('./config.json');
 const mongo = require('mongodb');
+
+const defaultConfig = config.dev;
+const environment = process.env.NODE_ENV || 'dev';
+const environmentConfig = config[environment];
+const fConfig = _.merge(defaultConfig, environmentConfig);
+
+global.gconfig = fConfig;
+
+console.log(`global.gconfig: ${JSON.stringify(global.gconfig, undefined, global.gconfig.json_indentation)}`);
+
+
 const MongoClient = mongo.MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://'+fConfig.serverip+':'+fConfig.port;
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
   if (err) throw err;
-  const db = client.db("testdb");
+  const db = client.db(fConfig.database);
   db.listCollections().toArray().then((docs) => {
       console.log('Available collections:');
       docs.forEach((doc, idx, array) => { console.log(doc.name) });
